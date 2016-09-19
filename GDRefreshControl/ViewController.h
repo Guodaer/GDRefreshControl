3744a3bc735fbfcb9b4925d7134a908b3ad0440b
guoda <1023251767@qq.com>
2016-09-19 18:27:22 +0800

    Initial Commit

---
+++ GDRefreshControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GDRefreshControl
+//
+//  Created by X-Designer on 16/9/19.
+//  Copyright © 2016年 Guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GDRefreshControl/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GDRefreshControl
+//
+//  Created by X-Designer on 16/9/19.
+//  Copyright © 2016年 Guoda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
